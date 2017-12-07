a0b716147a1ac2cd35ff7cbf2a7decd38267dcc7
jositoyomismo <jositoyomismo@hotmail.com>
2017-12-07 13:11:36 +0100

    Initial Commit

---
+++ jositoyomismo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jositoyomismo
+//
+//  Created by JOSE LUIS LOPEZ OLEA on 7/12/17.
+//  Copyright © 2017 JOSE LUIS LOPEZ OLEA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
